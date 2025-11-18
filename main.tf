fbb8e170a99dd81cc04b152bad5103767a74328b
Jefrin-Peter <143092078+Jeffy287@users.noreply.github.com>
2025-11-18 09:48:28 +0000

    test

--- main.tf
+++ main.tf
@@ -7,13 +7,5 @@ resource "aws_lambda_function" "test_lambda" {
   handler       = "index.handler"
   runtime       = "python3.8"
   filename      = "lambda_function_payload.zip"
-  role          = "arn:aws:iam::123456789012:role/execution_role:2" # <- Version-pinned ARN (should FAIL)
-}
-
-resource "aws_lambda_function" "ok_lambda" {
-  function_name = "ok_lambda"
-  handler       = "index.handler"
-  runtime       = "python3.8"
-  filename      = "lambda_function_payload.zip"
-  role          = "arn:aws:iam::123456789012:role/execution_role" # <- NOT version-pinned (should PASS)
+  role          = "arn:aws:iam::123456789012:role/execution_role:2" # <- This should FAIL
 }
